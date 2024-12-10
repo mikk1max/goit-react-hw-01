2fed74a64166d9417f150124342345a6ed65a5e0
Max <107277579+mikk1max@users.noreply.github.com>
2024-12-10 06:02:36 +0100

    feat: Profile component with styles

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Profile from "./components/Profile/Profile";
+import userData from "./userData.json";
+import "modern-normalize";
+import "./index.css";
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,29 @@
+import styles from "./Profile.module.css";
+
+export default function Profile({ name, tag, location, image, stats }) {
+  return (
+    <div className={styles.userCard}>
+      <div className={styles.userInfo}>
+        <img src={image} alt="User avatar" />
+        <p>{name}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
+
+      <ul className={styles.userStatsList}>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
